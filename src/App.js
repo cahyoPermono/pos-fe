eae1f3a0ef8ebbc1bb038cb550e6e68309852e99
Cahyo <cahyo.adi@imaniprima.com>
2020-11-24 19:17:17 +0700

    fixing header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/navbar/header.component";
+import Login from "./pages/login/login.component";
 import theme from "./theme/theme";
 
 class App extends React.Component {
@@ -10,7 +12,7 @@ class App extends React.Component {
 
     this.state = {
       shopName: "Toko Santoso",
-      isSignIn: true,
+      isSignIn: false,
       user: {
         name: "Rahmi Amiratus",
       },
@@ -20,14 +22,19 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div className="App">
-          <Header
-            shopName={this.state.shopName}
-            isSignIn={this.state.isSignIn}
-            user={this.state.user}
-          />
-          <h1>Body</h1>
-        </div>
+        <BrowserRouter>
+          {this.state.isSignIn ? (
+            <Header
+              shopName={this.state.shopName}
+              isSignIn={this.state.isSignIn}
+              user={this.state.user}
+            />
+          ) : null}
+          <switch>
+            <Route exact path="/" component={() => <h1>Home</h1>} />
+            <Route exact path="/login" component={Login} />
+          </switch>
+        </BrowserRouter>
       </ThemeProvider>
     );
   }
--- src/components/navbar/header.component.jsx
+++ src/components/navbar/header.component.jsx
@@ -69,9 +69,9 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: "hidden",
-    width: theme.spacing(7) + 1,
+    width: theme.spacing(6) + 1,
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1,
+      width: theme.spacing(8) + 1,
     },
   },
   toolbar: {
@@ -118,6 +118,7 @@ const Header = (props) => {
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
+        elevation="5"
       >
         <Toolbar>
           <IconButton
@@ -203,123 +204,5 @@ const Header = (props) => {
     </div>
   );
 };
-
-// const drawerWidth = 240;
-
-// const useStyle = makeStyles((theme) => ({
-//   toolbarMargin: {
-//     ...theme.mixins.toolbar,
-//     marginBottom: "3em",
-//   },
-//   buttonToolbar: {
-//     "&:hover": {
-//       backgroundColor: "transparent",
-//     },
-//     fontFamily: "Raleway",
-//     fontSize: "1em",
-//   },
-//   rightMenu: {
-//     marginLeft: "auto",
-//   },
-//   user: {
-//     fontFamily: "Pacifico",
-//     textTransform: "none",
-//   },
-//   drawer: {
-//     width: drawerWidth,
-//     flexShrink: 0,
-//     whiteSpace: "nowrap",
-//   },
-//   drawerOpen: {
-//     width: drawerWidth,
-//     transition: theme.transitions.create("width", {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   drawerClose: {
-//     transition: theme.transitions.create("width", {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//     overflowX: "hidden",
-//     width: theme.spacing(7) + 1,
-//     [theme.breakpoints.up("sm")]: {
-//       width: theme.spacing(9) + 1,
-//     },
-//   },
-//   menuButton: {
-//     marginRight: 36,
-//   },
-// }));
-
-// const Header = (props) => {
-//   const classes = useStyle();
-//   const [open, setOpen] = React.useState(false);
-
-//   const handleDrawerOpen = () => {
-//     setOpen(true);
-//   };
-
-//   const handleDrawerClose = () => {
-//     setOpen(false);
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <AppBar color="transparent" elevation={1}>
-//         <Toolbar>
-//           <Button disableRipple className={classes.buttonToolbar}>
-//             {props.shopName}
-//           </Button>
-//           <Divider
-//             orientation="vertical"
-//             flexItem
-//             className={classes.rightMenu}
-//           />
-//           {props.isSignIn ? (
-//             <Button className={classes.buttonToolbar}>
-//               <Avatar className={classes.user} style={{ marginRight: "5px" }}>
-//                 {props.user.name[0]}
-//               </Avatar>
-//               <Typography variant="button" className={classes.user}>
-//                 {props.user.name}
-//               </Typography>
-//               <ExpandMoreIcon />
-//             </Button>
-//           ) : (
-//             <Button className={classes.buttonToolbar}>Masuk</Button>
-//           )}
-//         </Toolbar>
-//       </AppBar>
-//       <Drawer
-//         variant="permanent"
-//         className={clsx(classes.drawer, {
-//           [classes.drawerOpen]: open,
-//           [classes.drawerClose]: !open,
-//         })}
-//         classes={{
-//           paper: clsx({
-//             [classes.drawerOpen]: open,
-//             [classes.drawerClose]: !open,
-//           }),
-//         }}
-//       >
-//       <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             onClick={handleDrawerOpen}
-//             edge="start"
-//             className={clsx(classes.menuButton, {
-//               [classes.hide]: open,
-//             })}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//       </Drawer>
-//       <div className={classes.toolbarMargin} />
-//     </React.Fragment>
-//   );
-// };
 
 export default Header;
